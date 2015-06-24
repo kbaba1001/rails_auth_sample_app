aa2f4b00379bcacfb78f909c9a8bd0c5e7a64f03
kbaba1001 <kbaba1001@users.noreply.github.com>
2015-06-25 07:39:36 +0900

    Add landing page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
-Rails.application.routes.draw do
-  scope module: 'users' do
-    root to: 'dashboards#show'
+require "monban/constraints/signed_in"
+require "monban/constraints/signed_out"
 
+Rails.application.routes.draw do
+  constraints Monban::Constraints::SignedIn.new do
+    root "users/dashboards#show", as: :dashboard
+  end
+
+  constraints Monban::Constraints::SignedOut.new do
+    root "landings#show"
+  end
+
+  scope module: 'users' do
     resource :session, only: %i(new create destroy)
     resources :password_resets, only: %i(new create)
 
