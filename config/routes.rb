0361df540ed5220fa0a21c31b3b4ea6ffa7c8e7d
kbaba1001 <kbaba1001@users.noreply.github.com>
2015-06-25 00:29:19 +0900

    rails g monban:password_reset

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   resource :session, only: %i(new create destroy)
-  resources :users, only: %i(new create)
+  resources :password_resets, only: %i(new create)
+
+  resources :users, only: %i(new create) do
+    resources :password_resets, only: %i(edit update)
+  end
 end
