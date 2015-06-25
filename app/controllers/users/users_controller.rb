06695d06747de65f8ccb413e667d8706e9b6053d
kbaba1001 <kbaba1001@users.noreply.github.com>
2015-06-26 00:04:29 +0900

    Use User has_secure_password
    ちょっと乱暴だが has_secure_password で password_confirmation 付きの
    ログインを実装しました。

--- app/controllers/users/users_controller.rb
+++ app/controllers/users/users_controller.rb
@@ -19,7 +19,7 @@ class Users::UsersController < Users::ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:email, :password)
+    params.require(:user).permit(:email, :password, :password_confirmation)
   end
 end
 
