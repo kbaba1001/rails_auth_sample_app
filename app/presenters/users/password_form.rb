cb4386f2939dd590a5bcb2c25b2168d4ed76b39c
kbaba1001 <kbaba1001@users.noreply.github.com>
2015-06-27 23:46:52 +0900

    typo

--- app/presenters/users/password_form.rb
+++ app/presenters/users/password_form.rb
@@ -1,6 +1,6 @@
 module Users
   class PasswordForm < Reform::Form
-    # TODO current_password と DB の値を症状する処理はどこに入れるのか？
+    # TODO current_password と DB の値を比較する処理はどこに入れるのか？
     #      Monban の authenticate メソッドでできる
     property :current_password, virtual: true, validates: {presence: true}
     property :new_password, virtual: true, validates: {presence: true, confirmation: {allow_blank: true}, length: {within: 6..20, allow_blank: true}}
