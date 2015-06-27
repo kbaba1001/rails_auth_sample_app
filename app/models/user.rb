82ade91b6a383847fbc7187851b4493f50c0d681
kbaba1001 <kbaba1001@users.noreply.github.com>
2015-06-27 12:30:15 +0900

    Use `Virtual Attributes(Reform 2.0)`
    see: https://github.com/apotonick/reform#virtual-attributes-reform-20

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,2 @@
 class User < ActiveRecord::Base
-  attr_accessor :password, :password_confirmation
 end
--- app/presenters/user_form.rb
+++ app/presenters/user_form.rb
@@ -1,5 +1,5 @@
 class UserForm < Reform::Form
   property :email, validates: {presence: true, uniqueness: true}
-  property :password, validates: {presence: true, confirmation: {allow_blank: true}, length: {within: 6..20, allow_blank: true}}
-  property :password_confirmation, validates: {presence: true}
+  property :password, virtual: true, validates: {presence: true, confirmation: {allow_blank: true}, length: {within: 6..20, allow_blank: true}}
+  property :password_confirmation, virtual: true, validates: {presence: true}
 end
