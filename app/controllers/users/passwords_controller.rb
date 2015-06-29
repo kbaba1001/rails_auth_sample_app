0c7d12e0639c4b83e928a489276fd34af50dc7c5
kbaba1001 <kbaba1001@users.noreply.github.com>
2015-06-29 23:57:52 +0900

    Add validate of authenticate current_password
    monban としてはコントローラで authenticate するはずだが、
    エラーメッセージの表示を考えると form で行うほうが良いと
    判断したためこのようになった。
    最適解ではないような気もする。
    
    Services::SignUp や Users::SignUpForm とのコードの重複が気になるが、
    ひとまずこのままとする。

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -7,13 +7,12 @@ module Users
     def update
       @form = PasswordForm.new(User.find(params[:user_id]))
 
-      # TODO ビジネスロジックを別のクラスに移動する
-      # TODO Services::SignUp とのコードの重複を排除する
       if @form.validate(params[:users_password])
         @form.save do |attributes|
           user = @form.model
           user.update(password_digest: Monban.hash_token(params[:users_password][:new_password]))
         end
+
         redirect_to root_path
       else
         render :edit
--- app/presenters/users/password_form.rb
+++ app/presenters/users/password_form.rb
@@ -1,9 +1,13 @@
 module Users
   class PasswordForm < Reform::Form
-    # TODO current_password と DB の値を比較する処理はどこに入れるのか？
-    #      Monban の authenticate メソッドでできる
     property :current_password, virtual: true, validates: {presence: true}
     property :new_password, virtual: true, validates: {presence: true, confirmation: {allow_blank: true}, length: {within: 6..20, allow_blank: true}}
     property :new_password_confirmation, virtual: true, validates: {presence: true}
+
+    validate do |form|
+      unless Monban.config.authentication_service.new(form.model, form.current_password).perform
+        errors.add(:current_password, :invalid)
+      end
+    end
   end
 end
