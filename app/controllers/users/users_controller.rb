417820a5915e82fc0f3ead4cd2861d17909e5bb9
kbaba1001 <kbaba1001@users.noreply.github.com>
2015-07-04 16:34:58 +0900

    cosmetics

--- app/controllers/users/users_controller.rb
+++ app/controllers/users/users_controller.rb
@@ -1,5 +1,5 @@
 class Users::UsersController < Users::ApplicationController
-  skip_before_action :require_login, only: [:new, :create, :edit, :update]
+  skip_before_action :require_login, only: %i(new create edit update)
 
   def new
     @form = Users::SignUpForm.new(User.new)
@@ -16,16 +16,16 @@ class Users::UsersController < Users::ApplicationController
 
   def update
     token = UserToken.find_by!(token: params[:token])
-
     user = token.user
 
     ActiveRecord::Base.transaction do
       Users::StateMachine.new(user).transition_to!(:active)
-
       token.destroy!
     end
 
     sign_in user
     redirect_to root_path
+  rescue
+    redirect_to root_path, alert: 'ERROR'
   end
 end
