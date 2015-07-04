62bab00f9ecfc4a55e2d0a720f7ec784c8287972
kbaba1001 <kbaba1001@users.noreply.github.com>
2015-07-04 16:03:21 +0900

    Add transaction
    reform内部では勝手にtransactionをはるようなことはしない

--- app/services/users/sign_up.rb
+++ app/services/users/sign_up.rb
@@ -8,15 +8,17 @@ module Users
       form = Users::SignUpForm.new(User.new)
 
       if form.validate(@users_sign_up_params)
-        # TODO fome.save は Transaction をはるかどうか調べる
-        form.save do |attributes|
-          user = form.model
-          # TODO emailアドレスの確認ができるまではこのアカウントで sing in できなくする。
-          # roll というか status カラムを作って、pandit とかで認可したい
-          user.update(password_digest: Monban.hash_token(attributes[:password]))
+        ActiveRecord::Base.transaction do
+          form.save do |attributes|
+            user = form.model
+            # TODO emailアドレスの確認ができるまではこのアカウントで sing in できなくする。
+            # roll というか status カラムを作って、pandit とかで認可したい
+            user.update!(password_digest: Monban.hash_token(attributes[:password]))
+            # TODO 初期状態の投入
 
-          user_token = UserToken.create(user: user)
-          UserTokenMailer.signup(user_token).deliver
+            user_token = UserToken.create!(user: user)
+            UserTokenMailer.signup(user_token).deliver
+          end
         end
       end
 
