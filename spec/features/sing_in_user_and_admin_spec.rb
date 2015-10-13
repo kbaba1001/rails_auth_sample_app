9ca89d06b0aa9a43503e9e2d427128a405f80f21
kbaba1001 <kbaba1001@users.noreply.github.com>
2015-10-13 22:43:45 +0900

    複数ログインのテスト

--- spec/features/sing_in_user_and_admin_spec.rb
+++ spec/features/sing_in_user_and_admin_spec.rb
@@ -1,11 +1,31 @@
 feature "Signing in" do
-  background do
-    User.create(email: 'user@example.com', password_digest: Monban.hash_token('password'))
-    Operator.create(email: 'admin1@example.com', password_digest: Monban.hash_token('password'))
-  end
+  let!(:user) {
+    User.create(
+      email: 'user@example.com',
+      password_digest: Monban.hash_token('password')
+    ).tap {|user|
+      Users::StateMachine.new(user).transition_to!(:active)
+    }
+  }
+  let!(:operator) { Operator.create(email: 'admin1@example.com', password_digest: Monban.hash_token('password')) }
 
-  scenario '' do
-    expect(User.count).to eq(1)
-    expect(Operator.count).to eq(1)
+  scenario 'ユーザーでサインイン中に管理者でサインインした場合、管理者の情報を取得できること' do
+    # ユーザーでサインイン
+    visit '/sessions/new'
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: 'password'
+    click_button 'Sign in'
+
+    expect(page).to have_content 'ログイン後マイページ'
+    expect(page).to have_content 'current_user: User'
+
+    # 管理者でサインイン
+    visit '/admin'
+    fill_in 'Email', with: operator.email
+    fill_in 'Password', with: 'password'
+    click_button 'sign in'
+
+    expect(page).to have_content '管理画面のダッシュボード'
+    expect(page).to have_content 'current_user: Operator'
   end
 end
