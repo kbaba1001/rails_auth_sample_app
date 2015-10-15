12f419f2aa5b942759e316d55c8c50222614a6b3
kbaba1001 <kbaba1001@users.noreply.github.com>
2015-10-16 00:15:29 +0900

    模索

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -1,40 +1,27 @@
 class Admin::ApplicationController < ApplicationController
   include Monban::ControllerHelpers
 
-  prepend_before_action {
-    Monban.configure do |config|
-      config.user_class = 'Operator'
+  # prepend_before_action {
+  #   Monban.configure do |config|
+  #     config.user_class = 'Operator'
 
-      config.sign_up_service = Monban::Services::SignUp
-      config.sign_in_service = Monban::Services::SignIn
+  #     config.sign_up_service = Monban::Services::SignUp
+  #     config.sign_in_service = Monban::Services::SignIn
 
-      config.no_login_redirect = admin_root_path
-    end
-  }
-
-  def signed_in?
-    operator = warden.user
-    operator.class.name == 'Operator' ? operator : nil
-  end
-
-
-  # helper_method :current_operator
-
-  # undef :current_user
-  # def current_operator
-  #   # FIXME なぜか User が入ってしまうようだ...。なんで?
-  #   @current_operator = warden.user(scope: :operator)
-  # end
+  #     config.no_login_redirect = admin_root_path
+  #   end
+  # }
 
   # def signed_in?
-  #   warden.user(scope: :operator)
-  # end
-
-  # def require_login
-  #   unless signed_in?
-  #     redirect_to admin_root_path
-  #   end
+  #   operator = warden.user
+  #   operator.class.name == 'Operator' ? operator : nil
   # end
 
   before_action :require_login
+
+  private
+
+  def authenticate_scope
+    :operator
+  end
 end
--- config/initializers/monban.rb
+++ config/initializers/monban.rb
@@ -1,6 +1,15 @@
-Monban.configure do |config|
+Monban.configure(:user) do |config|
   config.sign_up_service = Users::SignUp
   config.sign_in_service = Users::SignIn
 
   config.no_login_redirect = '/'
+end
+
+Monban.configure(:operator) do |config|
+  config.user_class = 'Operator'
+
+  config.sign_up_service = Monban::Services::SignUp
+  config.sign_in_service = Monban::Services::SignIn
+
+  config.no_login_redirect = admin_root_path
 end
