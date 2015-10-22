367f4bf8d0dfacae700d53aa31bcad3f1e1d336e
kbaba1001 <kbaba1001@users.noreply.github.com>
2015-10-23 00:47:11 +0900

    scope../..

--- app/services/users/sign_in.rb
+++ app/services/users/sign_in.rb
@@ -1,12 +1,13 @@
 module Users
   class SignIn
-    def initialize(user, warden)
+    def initialize(user, warden, scope = nil)
       @user = user
       @warden = warden
+      @scope = scope
     end
 
     def perform
-      @warden.set_user(@user) if Users::StateMachine.new(@user).current_state.active?
+      @warden.set_user(@user, scope: @scope) if Users::StateMachine.new(@user).current_state.active?
     end
   end
 end
