79f71edf28621854761d69cb297b676d5e24fa6e
kbaba1001 <kbaba1001@users.noreply.github.com>
2015-10-31 12:30:03 +0900

    remove config sign up/sign in service

--- config/initializers/monban.rb
+++ config/initializers/monban.rb
@@ -8,8 +8,5 @@ end
 Monban.configure(:operator) do |config|
   config.user_class = 'Operator'
 
-  config.sign_up_service = Monban::Services::SignUp
-  config.sign_in_service = Monban::Services::SignIn
-
   config.no_login_redirect = {controller: 'admin/sessions', action: 'new'}
 end
